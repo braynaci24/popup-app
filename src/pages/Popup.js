cc144c20104e204fe5f137fc80a0937980c5b823
Muhammet Emin Aynacı <aynacimuhammetemin@gmail.com>
2022-11-23 23:17:17 +0300

    List Page

--- src/pages/Popup.js
+++ src/pages/Popup.js
@@ -7,11 +7,13 @@ function Popup() {
     const [width, setWidth] = useState();
     const [height, setHeight] = useState();
     const [title, setTitle] = useState();
-    const [popBgColor, setColor] = useState();
+    const [popTitleTextColor, setTitleColor] = useState("white");
+    const [popBgColor, setColor] = useState("black");
     const [horizontalTitle, setHorizontalTitle] = useState();
+    const [verticalTitle, setVerticalTitle] = useState();
 
     function PopupWidth (popWidth){
-        if(popWidth > 1200){
+        if(popWidth > 500){
             toast.error("Length must be less than 1200")
             setWidth(0)
         }else
@@ -29,62 +31,92 @@ function Popup() {
     function PopColor (popColor){
         setColor(popColor)
     }
+    function popTitleColor (titleColor){
+        setTitleColor(titleColor)
+    }
 
     function PopHoriz (horiz){
         setHorizontalTitle(horiz)
     }
 
+    function PopVertical (vertical){
+        setVerticalTitle(vertical)
+    }
+
     function setLocalStorage () {
+        var d = new Date();
+        var time = d.getHours() + ":" + d.getMinutes()
+        var month = d.getMonth()+1;
+        var day = d.getDate();
+      
+      var output = d.getFullYear() + '/' +
+          (month<10 ? '0' : '') + month + '/' +
+          (day<10 ? '0' : '') + day;
+      
         let pop = {
             width: width,
             height: height,
             title: title,
             bgColor: popBgColor,
-            titleAlignmet: horizontalTitle
+            titleColor: popTitleTextColor,
+            titleVerticalAlignmet: verticalTitle,
+            titleAlignmet: horizontalTitle,
+            creationDate: `${output} - ${time}`
         }
-        storagePopup.push(pop)
-        localStorage.setItem('popup', JSON.stringify(storagePopup))
-        toast.success(title +" added to your list")
+        if(width && height){
+            storagePopup.push(pop)
+            localStorage.setItem('popup', JSON.stringify(storagePopup))
+            toast.success(title +" added to your list")
+        }
+        else 
+            toast.error("Width and length are required")
+
     }
 
   return (
       <div className="flex">
         <div className="component-container">
             <Toaster />
-            <div id="popup-controller" className="flex">
-                <div className="mr-40">
+            <div id="popup-controller" className="grid">
+                <div className="flex">
                     <span className="mr-10">Width</span>
                     <input type="number" className="popup-admin-input pl-10" onChange={popWidth => PopupWidth(popWidth.target.value)}/>
                 </div>
-                <div className="mr-40">
+                <div className="flex">
                     <span className="mr-10">Height</span>
                     <input type="number" className="popup-admin-input pl-10" onChange={popHeight => PopupHeight(popHeight.target.value)} disabled={!width}/>
                 </div>
-                <div className="mr-40">
-                    <span className="mr-10 ml-50">Title</span>
+                <div className="flex">
+                    <span className="mr-10">Title</span>
                     <input type="text" className="popup-admin-input pl-10" onChange={popTitle => PopupTitle(popTitle.target.value)} disabled={!width}/>
                 </div>
-                <div className="flex mr-40">
-                    <span className="mr-10 ml-50">Color</span>
+                <div className="flex">
+                    <span className="mr-10">Color</span>
                     <input type="color" className="popup-admin-color pl-10" onChange={popColor => PopColor(popColor.target.value)} disabled={!width}/>
                 </div>
-                <div className="flex mr-40">
-                    <span className="mr-10 ml-50">Title Position Horizontal</span>
-                    <input type="range" id="points" name="points" min="0" max={width} onChange={horiz => PopHoriz(horiz.target.value)}  disabled={!height}/>
+                <div className="flex">
+                    <span className="mr-10">Title Color</span>
+                    <input type="color" className="popup-admin-color pl-10" onChange={titleColor => popTitleColor(titleColor.target.value)} disabled={!width}/>
+                </div>
+                <div className="flex">
+                    <span className="mr-10">Title Position Horizontal</span>
+                    <input type="range" id="points" name="points" min="0" max={width - 32} onChange={horiz => PopHoriz(horiz.target.value)}  disabled={!height}/>
+                </div>
+                <div className="flex">
+                    <span className="mr-10">Title Position Vertical</span>
+                    <input type="range" id="points" name="points" min="0" max={height - 20} onChange={vertical => PopVertical(vertical.target.value)}  disabled={!height}/>
                 </div>
             </div>
             <button id="popup-create" className="mt-40" onClick={setLocalStorage}>Add Popup List</button>
-            <div className="flex" id="content">
                 {
                     width ? 
-                    <div id="popup" className="mt-140" style={{width: + width + "px", height: + height + "px", backgroundColor: popBgColor, position: "relative"}} >
-                    <span  style={{position: "absolute", left: horizontalTitle + "px"}}> { title ? title : "Title"}</span>
+                    <div id="popup" className="mt-140" style={{width: + width + "px", height: + height + "px", position: "relative", backgroundColor: popBgColor}} >
+                    <span  style={{position: "absolute", left: horizontalTitle + "px", top: verticalTitle + "px", color: popTitleTextColor}}> { title ? title : "Title"}</span>
                     </div> 
                     : 
                     <></>
                 }
                 
-            </div>
         </div>
     </div>
     
--- src/pages/PopupList.js
+++ src/pages/PopupList.js
@@ -1,21 +1,53 @@
-import React from 'react'
-
+import { useRef, useState} from 'react'
 function PopupList() {
   let localPopData = JSON.parse(localStorage.getItem('popup'))
+  const [active, setActive] = useState(false);
+  let cellRef = useRef([]);
+
+  function handleCell (index) {
+    setActive(!active);
+    if(cellRef.current[index].style.display === "none"){
+      cellRef.current[index].style.display="block";
+    }else {
+      cellRef.current[index].style.display="none";
+    }
+  }
 
   return (
-    <div style={{display: "grid", marginLeft: "25%", gridTemplateColumns: "auto auto auto"}}>
+    <div className="popup-list-container" style={{ marginLeft: "25%", width: "64%"}}>
+      <table style={{width: "100%"}}>
     {
     localPopData && localPopData.map((item, index) => {
         return(
-          <div key={index} id="popup" className="mt-140 mr-40" style={{width: + item.width + "px", height: + item.height + "px", backgroundColor: item.bgColor, position: "relative"}} >
-              <span  style={{position: "absolute", left: item.titleAlignmet + "px"}}> { item.title ? item.title : "Title"}</span>
-          </div> 
-        )
+          <div key={index}>
+              <tr>
+                <th className="table-item-title">Title</th>
+                <th className="table-item-creation-date">Creation date</th>
+                <th>Show</th>
+              </tr>
+              <tr>
+                <td>
+                  { item.title ? item.title : "Title"}
+                  <div id="popup" ref={(ele)=> cellRef.current[index] = ele} className="mt-140 mr-40" style={{display: "none", width: + item.width + "px", height: + item.height + "px", backgroundColor: item.bgColor, position: "absolute", margin: "0 auto", left: "0", right: "0"}} >
+                      <span style={{position: "absolute", left: item.titleAlignmet + "px", color: item.titleColor, top: item.titleVerticalAlignmet + "px"}}> { item.title ? item.title : "Title"}</span>
+                  </div>
+                </td>
+                <td>
+                  {item.creationDate}
+                  
+                </td>
+                <td><button onClick={()=> handleCell(index)}> { active ? "Hide" : "Show"}</button></td>
+              </tr>
+              <tr>
+              </tr>
+            
+          </div>
+          
+          )
     })
-    }
+  }
+  </table>
     </div>
   )
 }
-
 export default PopupList
