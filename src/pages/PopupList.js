af426c93e3f8dd343f6156fe313da0ca59f9c791
braynaci <aynacimuhammetemin@gmail.com>
2022-12-01 22:46:06 +0300

    Clear

--- src/pages/PopupList.js
+++ src/pages/PopupList.js
@@ -1,13 +1,13 @@
-import { useRef, useState } from 'react'
+/* import { useRef } from 'react' */
 import {Link} from "react-router-dom"
 function PopupList() {
   let localPopData = JSON.parse(localStorage.getItem('popup'))
-  let refs = useRef([]);
-  const [currentPopup, setCurrentPopup] = useState()
+/*   let refs = useRef([]); */
+  /* const [currentPopup, setCurrentPopup] = useState() */
 
-  function handleCell (index) {
+  /* function handleCell (index) {
     setCurrentPopup(index)
-  }
+  } */
 
   return (
     <div className="popup-list-container" style={{ marginLeft: "25%", width: "64%"}}>
@@ -26,9 +26,9 @@ function PopupList() {
           <tr key={index}>
             <td>
               { item.title ? item.title : "Title"}
-              <div id="popup" ref={(ele)=> refs.current[index] = ele} className="mt-140 mr-40" style={{  display: currentPopup != index ? "none" : "block" , width: + item.width + "px", height: + item.height + "px", backgroundColor: item.bgColor, position: "absolute", margin: "0 auto", left: "0", right: "0"}} >
+              {/* <div id="popup" ref={(ele)=> refs.current[index] = ele} className="mt-140 mr-40" style={{  display: currentPopup != index ? "none" : "block" , width: + item.width + "px", height: + item.height + "px", backgroundColor: item.bgColor, position: "absolute", margin: "0 auto", left: "0", right: "0"}} >
                   <span style={{position: "absolute", left: item.titleAlignmet ? item.titleAlignmet + "px" : 44 + "%", color: item.titleColor, top: item.titleVerticalAlignmet + "px"}}> { item.title ? item.title : "Title"}</span>
-              </div>
+              </div> */}
             </td>
             <td>
               {item.creationDate}
